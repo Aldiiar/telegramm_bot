9f69d56649bc2587806d31e97c150420a541552e
Aldiiar <aldiarm27@gmail.com>
2023-01-22 19:40:59 +0600

    добавил FSM в телеграмм боте

--- handlers/avto_buying.py
+++ handlers/avto_buying.py
@@ -9,37 +9,30 @@ async def car_c(message: types.Message):
     '''
     СПИСОК АBTО
     '''
-    photo1 = open('images/6ff264as-960.jpg', 'rb')
-    photo2 = open('images/hqdefault.jpg', 'rb')
     await message.answer(text='Все автомобили класса -С-')
-    await message.answer_photo(photo1)
-    await message.answer(text='Авто №1', reply_markup=car_kb)
-    await message.answer_photo(photo2)
-    await message.answer(text='Авто №2', reply_markup=car_kb)
+    await message.answer_photo((open('images/6ff264as-960.jpg', 'rb')), caption='Авто №1',
+                               reply_markup=car_kb)
+    await message.answer_photo((open('images/hqdefault.jpg', 'rb')), caption='Авто №2',
+                               reply_markup=car_kb)
 
 
 async def car_e(message: types.Message):
     '''
     СПИСОК АBTО
     '''
-    photo3 = open('images/e124.jpg', 'rb')
-    photo4 = open('images/e140.jpg', 'rb')
     await message.answer(text='Все автомобили класса -E-')
-    await message.answer_photo(photo3)
-    await message.answer(text='Авто №1', reply_markup=car_kb)
-    await message.answer_photo(photo4)
-    await message.answer(text='Авто №2', reply_markup=car_kb)
+    await message.answer_photo((open('images/e124.jpg', 'rb')), caption='Авто №1',
+                               reply_markup=car_kb)
+    await message.answer_photo((open('images/e140.jpg', 'rb')), caption='Авто №2',
+                               reply_markup=car_kb)
 
 
 async def car_s(message: types.Message):
     '''
     СПИСОК АBTО
     '''
-    photo5 = open('images/mazda.jpeg', 'rb')
-    photo6 = open('images/sub.jpg', 'rb')
     await message.answer(text='Все автомобили класса -S-')
-    await message.answer_photo(photo5)
-    await message.answer(text='Авто №1', reply_markup=car_kb)
-    await message.answer_photo(photo6)
-    await message.answer(text='Авто №2', reply_markup=car_kb)
-
+    await message.answer_photo((open('images/mazda.jpeg', 'rb')), caption='Авто №1',
+                               reply_markup=car_kb)
+    await message.answer_photo((open('images/sub.jpg', 'rb')), caption='Авто №2',
+                               reply_markup=car_kb)
--- main.py
+++ main.py
@@ -2,31 +2,51 @@ from aiogram import Bot, Dispatcher, executor
 from dotenv import load_dotenv
 from os import getenv
 from aiogram.dispatcher.filters import Text
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 '''импорт функций'''
 from handlers.answer import qwerty
 from handlers.start import start_command
 from handlers.buy import buy_command
 from handlers.sell import sell_command
-from handlers.avto_buying import car_c
-from handlers.avto_buying import car_e
-from handlers.avto_buying import car_s
+from handlers.avto_buying import car_c, car_e, car_s
+
+'''импорт функций формы'''
+from handlers.sell import Form
+from handlers.sell import cansel_handler
+from handlers.sell import (
+    form_start,
+    name_process,
+    phone_number_process,
+    car_brand_process,
+    car_price_process,
+    address_process
+)
 
 
 load_dotenv()
 bot = Bot(getenv('BOT_TOKEN'))
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 
 dp.register_message_handler(start_command, commands=['start'])
 dp.register_callback_query_handler(buy_command, text='buy_command')
-dp.register_callback_query_handler(sell_command, text='sell_command')
 dp.register_message_handler(car_c, Text(equals='C class'))
 dp.register_message_handler(car_e, Text(equals='E class'))
 dp.register_message_handler(car_s, Text(equals='S class'))
+dp.register_callback_query_handler(sell_command, text='sell_command')
+dp.register_message_handler(form_start, commands=['form'])
+dp.register_message_handler(cansel_handler, state='*', commands='cancel')
+dp.register_message_handler(cansel_handler, Text(equals='cansel', ignore_case=True), state='*')
+dp.register_message_handler(name_process, state=Form.name)
+dp.register_message_handler(phone_number_process, state=Form.phone_number)
+dp.register_message_handler(car_brand_process, state=Form.car_brand)
+dp.register_message_handler(car_price_process, state=Form.car_price)
+dp.register_message_handler(address_process, state=Form.address)
 dp.register_message_handler(qwerty)
+
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
